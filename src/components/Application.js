df9d96cfb6cf92854a0fe8293aca8ebd9dacc2c0
Sam <sammynarduzzi@gmail.com>
2023-03-20 15:04:25 -0700

    create home page for users with css

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,12 +3,15 @@ import React from "react";
 import "components/Application.scss";
 import AdminPortal from "pages/AdminPortal";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserHome from "pages/UserHome";
 
 export default function Application(props) {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/admin" element={<AdminPortal />}></Route>
+        <Route path="/" element={<UserHome />}></Route>
+
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/User/UserInput.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import 'components/User/UserInput.scss';
+
+
+function UserInput() {
+
+  return (
+    <>
+    <main class="main">
+      <h1 class="welcome"> WELCOME TO </h1>
+      <h1 class="restaurant"> *RESTAURANT* </h1>
+      <section>
+        <div class="input-field">
+          <form action="/admin">
+            <div>
+              <input type="text" class="form-control" name="name" placeholder="Name" required="required"/>
+            </div>
+
+            <div>
+              <input type="text" class="form-control" name="table" placeholder="Table Number" required="required"/>
+            </div>
+
+            <div>
+              <input type="tel" class="form-control" name="phone" placeholder="Phone Number" required="required"/>
+            </div>
+
+            <div>
+              <input type="submit" class="btn btn-primary btn-block btn-lg" value="Continue to Menu"/>
+            </div>
+          </form>
+        </div>
+      </section>
+      </main>
+    </>
+  );
+}
+
+export default UserInput;
+
