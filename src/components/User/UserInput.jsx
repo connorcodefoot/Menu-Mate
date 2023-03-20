61142aa5ff3815bc78453b186b7b3b10413c5556
Sam <sammynarduzzi@gmail.com>
2023-03-20 15:54:19 -0700

    add state to user home page

--- src/components/User/UserInput.jsx
+++ src/components/User/UserInput.jsx
@@ -1,35 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import 'components/User/UserInput.scss';
 
 
 function UserInput() {
+  const [state, setState] = useState({
+    name: "",
+    table: "",
+    phone: ""
+  });
+
+  const handleChange = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <>
-    <main class="main">
-      <h1 class="welcome"> WELCOME TO </h1>
-      <h1 class="restaurant"> *RESTAURANT* </h1>
-      <section>
-        <div class="input-field">
-          <form action="/menu">
-            <div>
-              <input type="text" class="form-control" name="name" placeholder="Name" required="required"/>
-            </div>
+      <main class="main">
+        <h1 class="welcome"> WELCOME TO </h1>
+        <h1 class="restaurant"> *RESTAURANT* </h1>
+        <section>
+          <div class="input-field">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="name"
+                  placeholder="Name"
+                  value={state.name}
+                  onChange={handleChange} required
+                />
+              </div>
 
-            <div>
-              <input type="text" class="form-control" name="table" placeholder="Table Number" required="required"/>
-            </div>
+              <div>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="table"
+                  placeholder="Table Number"
+                  value={state.table}
+                  onChange={handleChange} required
+                />
+              </div>
 
-            <div>
-              <input type="tel" class="form-control" name="phone" placeholder="Phone Number" required="required"/>
-            </div>
+              <div>
+                <input
+                  type="tel"
+                  class="form-control"
+                  name="phone"
+                  placeholder="Phone Number"
+                  value={state.phone}
+                  onChange={handleChange} required
+                />
+              </div>
 
-            <div>
-              <input type="submit" class="btn btn-primary btn-block btn-lg" value="Continue to Menu"/>
-            </div>
-          </form>
-        </div>
-      </section>
+              <div>
+                <input
+                  type="submit"
+                  class="btn btn-primary btn-block btn-lg" 
+                  value="Continue to Menu"
+                  onClick={() => {
+                    window.location.href = '/menu';
+                  }}
+                />
+              </div>
+            </form>
+          </div>
+        </section>
       </main>
     </>
   );
