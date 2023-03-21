10fefa3b7a56f3c373e305b956069765af8d0c20
Connor Broadfoot <cgbroadfoot@gmail.com>
2023-03-21 08:41:12 -0700

    new page to show components for user menu and user menu items

---
+++ src/pages/UserMenu.jsx
@@ -0,0 +1,12 @@
+import UserMenu from "components/User/UserMenu";
+import React from "react";
+
+export default function UserMenuView() {
+  
+  return (
+    <>
+    <UserMenu />
+    </>
+
+  )
+}
