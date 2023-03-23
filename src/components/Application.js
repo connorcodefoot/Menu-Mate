d3986314fd7c23e212361ebc4a499faf7d3946f3
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-03-22 19:35:03 -0700

    Cleans up menu routes

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,6 @@ import AdminOrders from "pages/AdminOrders"
 import AdminPortal from "pages/AdminPortal";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserHome from "pages/UserHome";
-import MenuItem from "./MenuItem";
 import UserMenu from "pages/UserMenu";
 import AdminMenu from "pages/AdminMenu";
 
@@ -18,7 +17,6 @@ export default function Application(props) {
         <Route path="/" element={<UserHome />}></Route>
         <Route path="/user/menu" element={<UserMenu />}></Route>
         <Route path="/admin/menu" element={<AdminMenu />}></Route>
-        <Route path="/menu" element={<MenuItem />}></Route>
         <Route path="admin/orders" element={<AdminOrders />}></Route>
       </Routes>
     </BrowserRouter>
