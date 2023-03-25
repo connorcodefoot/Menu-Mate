c0617b2a1464ef47db66dc39a3f91af50c54d0ff
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-03-25 14:19:23 -0700

    Fixes bug

--- src/components/Admin/AdminOrderView.jsx
+++ src/components/Admin/AdminOrderView.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
-import backgroundImage from "/Users/sam/Menu-Mate/src/admin-background.png"
+
 
 export default function AdminOrderView(props) {
   const [show, setShow] = useState(false);
