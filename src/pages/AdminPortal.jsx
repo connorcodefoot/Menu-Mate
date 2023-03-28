66438457e4fe14d8e603c00d2732ee394e7915c8
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-03-28 13:07:52 -0700

    Cleans up redundant files

--- src/pages/AdminPortal.jsx
+++ src/pages/AdminPortal.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "components/Admin/AdminNav";
-import Menu from "components/Menu";
 
 export default function AdminPortal() {
   
@@ -8,7 +7,6 @@ export default function AdminPortal() {
     <>
     < Navbar />
     <h1> This is the admin portal</h1>
-    <Menu></Menu>
     </>
 
   )
